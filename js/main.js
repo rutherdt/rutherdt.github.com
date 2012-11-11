774250583f30f4d481bb93b45c25c87bea072244
Leander Herzog <info@leanderherzog.ch>
2012-11-11 02:12:43 +0100

    hires images for slideshows

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
-/* Author:
+$(document).ready(function() {
 
-*/
+  $("img").click(function(){
+    var images=$('img');
+    var slides=new Array();
+    var num=images.length;
+    for(var i=0;i<num;i++){
+      var e=$(images[i]).attr('src');
+      var tmp={image: e, title:"yeah"};
+      slides[i]=tmp;
+    }
+        $.supersized({
+          slide_interval          :   3000,   // Length between transitions
+          transition              :   1,      // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+          transition_speed    : 700,    // Speed of transition
+          slide_links       : 'blank',  // Individual links for each slide (Options: false, 'num', 'name', 'blank')
+          slides          : slides
+        });
+
+
+  });
+});
+
+
+
--- tasks/render.sh
+++ tasks/render.sh
@@ -10,6 +10,7 @@ for folder in $folders;do
   newfolder=$out/$CLEAN
   mkdir -p $newfolder
   convert -verbose -thumbnail 600x -quality 85% $folder/* $newfolder/ruth-erdt-$CLEAN-%d-600.jpg
+  convert -verbose -thumbnail 1600x -quality 85% $folder/* $newfolder/ruth-erdt-$CLEAN-%d-1600.jpg
   convert -thumbnail 200x -quality 85% $folder/* $newfolder/ruth-erdt-$CLEAN-%d-200.jpg
 done
 cd ..

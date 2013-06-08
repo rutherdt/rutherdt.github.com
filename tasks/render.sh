d46839670a3a973f1eeffed4552ab10e377e3182
Leander Herzog <info@leanderherzog.ch>
2013-06-08 23:48:17 +0200

    bigger images

--- tasks/render.sh
+++ tasks/render.sh
@@ -9,9 +9,9 @@ for folder in $folders;do
   CLEAN=${folder//[^a-zA-Z0-9-]/}
   newfolder=$out/$CLEAN
   mkdir -p $newfolder
-  convert -verbose -thumbnail 600x -quality 85% $folder/* $newfolder/ruth-erdt-$CLEAN-%d-600.jpg
-  convert -verbose -thumbnail 1200x -quality 85% $folder/* $newfolder/ruth-erdt-$CLEAN-%d-600@2x.jpg
-  convert -verbose -thumbnail 1600x -quality 85% $folder/* $newfolder/ruth-erdt-$CLEAN-%d-1600.jpg
+  convert -verbose -thumbnail 700x -quality 85% $folder/* $newfolder/ruth-erdt-$CLEAN-%d-700.jpg
+  convert -verbose -thumbnail 1400x -quality 85% $folder/* $newfolder/ruth-erdt-$CLEAN-%d-700@2x.jpg
+  convert -verbose -thumbnail 1800x -quality 85% $folder/* $newfolder/ruth-erdt-$CLEAN-%d-1800.jpg
   convert -thumbnail 200x -quality 85% $folder/* $newfolder/ruth-erdt-$CLEAN-%d-200.jpg
 done
 cd ..
